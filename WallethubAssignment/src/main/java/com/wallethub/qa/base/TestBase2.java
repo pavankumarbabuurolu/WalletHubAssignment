3256c75d6756fbbf7d2835727f032f5c3258dbce
babuurolupavan <babuurolupavankumar@gmail.com>
2022-03-26 15:35:00 +0530

    commit message1

---
+++ WallethubAssignment/src/main/java/com/wallethub/qa/base/TestBase2.java
@@ -0,0 +1,13 @@
+package com.wallethub.qa.base;
+
+public class TestBase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Pavan");
+		
+		
+	}
+
+}
